77a45b09a2f451161309157877e218bf24980867
Sahi7676 <2022sp93070@wilp.bits-pilani.ac.in>
2023-03-26 15:54:23 +0530

    initial commit

---
+++ src/test/java/com/sahil/workflow/WorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahil.workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
